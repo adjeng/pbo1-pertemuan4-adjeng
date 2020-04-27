636b94deb769b762eec0dddae4333d68b5071b7e
adjeng <sekariniadjeng@gmail.com>
2020-04-27 21:37:29 +0800

    Perulangan While Bernilai True

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
 	public static void main(String[] args) {
-		while(isiGelas != penuh){
-			isiAir();
+		int isiGelas = 0;
+		int penuh = 225;
+		
+		System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+		System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+		
+		while(isiGelas  != penuh){
+			isiGelas++;
+			System.out.println("Sedang mengisi gelas..");
+			System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
 		}
+		
+		
+		System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
 	}
 }
